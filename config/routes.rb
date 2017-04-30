2a9fc44028958092ff147ae45901a5f687f50389
Fatos Morina <fatosi.morina@gmail.com>
2017-04-30 18:58:55 +0200

    Add like to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pictures
+  resources :pictures do
+  	member do
+  	  put "like", to: "picture#upvote"
+  	end
+  end		
   root "pictures#index"
 end
