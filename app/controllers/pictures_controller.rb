185e4fdf320d57f3ddc860b190b4357ba6428e60
Fatos Morina <fatosi.morina@gmail.com>
2017-04-30 11:41:29 +0200

    Show and Index page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,21 +1,36 @@
 class PicturesController < ApplicationController
 
+	before_action :find_picture, only: [:show, :edit, :update, :destroy]
+
+
 	def index
+	  
 	end
+
+	def show
+	end	
 	
 	def new
 	  @picture = Picture.new
 	end
-	
+
 	def create
 	  @picture = Picture.new(picture_params)
-	end	
-
+	  if @picture.save
+	    redirect_to @picture, notice:	'Picture was posted!'
+	  else
+	  	render 'new'
+	  end	
+	end
 
 	private
 
 	def picture_params
 	 params.require(:picture).permit(:title, :description)	
+	end
+
+	def find_picture
+	 @picture = Picture.find(params[:id])	
 	end
 
 end
