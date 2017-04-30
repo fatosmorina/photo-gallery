6703f6cd904403bdabfc291bcc5024b2df9bff32
Fatos Morina <fatosi.morina@gmail.com>
2017-04-30 12:40:08 +0200

    Generate devise User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430085646) do
+ActiveRecord::Schema.define(version: 20170430103720) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
